4f5cddf91dc49babe1aa09aaa54f00ed47485b02
OleksandrGrubyi <ghrubyi@ukr.net>
2020-11-06 03:06:24 +0200

    fix bugs

--- src/js/functions.js
+++ src/js/functions.js
@@ -6,7 +6,7 @@ import { butOn } from './ref';
 
 
 export const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min)
 };
 
 
